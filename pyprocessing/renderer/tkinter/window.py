dff6fdb75132e167a8091e6d7f342d13bcc8db6d
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2020-09-20 20:42:40 +0100

    Fix outlines

--- pyprocessing/renderer/tkinter/window.py
+++ pyprocessing/renderer/tkinter/window.py
@@ -121,9 +121,3 @@ class Window(tk.Frame):
             x, y, x + 1, y + 1, fill=self.stroke.hex
         )
         self.queued_actions.append(action)
-
-    def set_point(self, x, y):
-        action = Action(self.canvas, 'create_line',
-            x, y, x+1, y+1, fill=self.stroke.hex
-        )
-        self.queued_actions.append(action)
