361960c96caca8e2726c5bb5eea10ddbc4e499ae
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2020-09-20 23:20:20 +0100

    Fix Styling

--- pyprocessing/primitives_2d.py
+++ pyprocessing/primitives_2d.py
@@ -11,11 +11,15 @@ def rect(x, y, width, height, *args):
     if len(args) == 0:
         pp.windows.set_rectangle(x, y, width, height)
     elif len(args) == 1:
-        pp.windows.set_rounded_rectangle(x, y, width, height,
-                                         args[0], args[0], args[0], args[0])
+        pp.windows.set_rounded_rectangle(
+            x, y, width, height,
+            args[0], args[0], args[0], args[0]
+        )
     elif len(args) == 4:
-        pp.windows.set_rounded_rectangle(x, y, width, height,
-                                         args[0], args[1], args[2], args[3])
+        pp.windows.set_rounded_rectangle(
+            x, y, width, height,
+            args[0], args[1], args[2], args[3]
+        )
     else:
         raise TypeError
 
