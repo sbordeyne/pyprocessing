cfc4574c5d2f65a7f95016f3b406475109c5246b
Dogeek <Dogeek@users.noreply.github.com>
2020-09-23 13:58:26 +0200

    PImage factory functions, and PImage filters

--- pyprocessing/image.py
+++ pyprocessing/image.py
@@ -1,6 +1,17 @@
+from enum import Enum, IntEnum
+import os.path
 import pathlib
 
-from PIL import Image, ImageTk, ImageQt
+from PIL import Image, ImageTk, ImageQt, ImageFilter, ImageOps
+import requests
+
+from pyprocessing.utils import url_re
+
+
+__all__ = (
+    'PImage', 'FilterKind', 'ImageFormat',
+    'create_image',
+)
 
 
 class _PixelsProxy(list):
@@ -9,9 +20,80 @@ class _PixelsProxy(list):
         super().__init__(self.image.getdata())
 
     def __setitem__(self, index, value):
-        x = index // self._image.width
-        y = index % self._image.width
+        x = index % self._image.width
+        y = index // self._image.width
         self._image.putpixel((x, y), value)
+
+
+class ImageFormat(IntEnum):
+    RGB = 0
+    ARGB = 1
+    ALPHA = 2
+
+
+class FilterKind(IntEnum):
+    THRESHOLD = 0
+    GRAY = 1
+    OPAQUE = 2
+    INVERT = 3
+    POSTERIZE = 4
+    BLUR = 5
+    ERODE = 6
+    DILATE = 7
+
+
+class _FilterKindParamDefault(Enum):
+    THRESHOLD = 0.5
+    GRAY = None
+    OPAQUE = None
+    INVERT = None
+    POSTERIZE = None
+    BLUR = 1
+    ERODE = None
+    DILATE = None
+
+
+def _threshold_filter(image, threshold):
+    if isinstance(threshold, float):
+        threshold *= 256
+    gray = image.convert(mode='L')
+    mapping = [0, 255]
+    for i, pixel in enumerate(gray.getdata()):
+        x = i % gray.width
+        y = i // gray.width
+        gray.putpixel((x, y), mapping[pixel <= threshold])
+    return gray
+
+
+def _gray_filter(image):
+    return ImageOps.grayscale(image)
+
+
+def _opaque_filter(image):
+    return image.putalpha(0)
+
+
+def _invert_filter(image):
+    return ImageOps.invert(image)
+
+
+def _posterize_filter(image, bits):
+    if bits > 8:
+        bits = bits // 8
+    bits = min(max(bits, 1), 8)
+    return ImageOps.posterize(image, bits)
+
+
+def _blur_filter(image, radius):
+    return image.filter(ImageFilter.GaussianBlur(radius))
+
+
+def _erode_filter(image):
+    return image.filter(ImageFilter.MinFilter(3))
+
+
+def _dilate_filter(image):
+    return image.filter(ImageFilter.MaxFilter(3))
 
 
 class PImage:
@@ -70,8 +152,27 @@ class PImage:
     def mask(self):
         pass
 
-    def filter(self):
-        pass
+    def filter(self, kind, param=None):
+        filters = {
+            FilterKind.THRESHOLD: _threshold_filter,
+            FilterKind.GRAY: _gray_filter,
+            FilterKind.OPAQUE: _opaque_filter,
+            FilterKind.INVERT: _invert_filter,
+            FilterKind.POSTERIZE: _posterize_filter,
+            FilterKind.BLUR: _blur_filter,
+            FilterKind.ERODE: _erode_filter,
+            FilterKind.DILATE: _dilate_filter,
+        }
+        if kind not in filters:
+            raise ValueError(f'Kind {kind} not in available filters.')
+        image_filter = filters[kind]
+        arg = _FilterKindParamDefault[FilterKind(kind).name].value
+        if arg is None and kind == FilterKind.POSTERIZE:
+            raise ValueError('Kind FilterKind.POSTERIZE requires an argument.')
+
+        if arg is None:
+            return image_filter(self._image)
+        return image_filter(self._image, arg)
 
     def copy(self):
         pass
@@ -79,5 +180,29 @@ class PImage:
     def blend(self):
         pass
 
-    def save(self):
-        pass
+    def save(self, filename):
+        fname, ext = os.path.splitext(filename)
+        if not ext:
+            ext = 'tiff'
+        filename = f"{fname}.{ext}"
+        self._image.save(filename)
+        return True
+
+
+def create_image(width, height, format):
+    if format == ImageFormat.RGB:
+        format = 'RGB'
+    elif format == ImageFormat.ARGB:
+        format = 'RGBA'
+    else:
+        format = 'LA'
+    pil_image = Image.new(format, (width, height))
+    return PImage(pil_image)
+
+
+def load_image(url, extension=None):
+    if url_re.match(url):
+        pil_image = Image.open(requests.get(url, stream=True).raw)
+    else:
+        pil_image = Image.open(url)
+    return PImage(pil_image)
--- pyprocessing/utils.py
+++ pyprocessing/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 class SingletonMeta(type):
     _instances = {}
 
@@ -5,3 +8,11 @@ class SingletonMeta(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+url_re = re.compile((
+    r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+'
+    r'[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+'
+    r'[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))'
+    r'[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
+))
